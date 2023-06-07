df050ff012cbe23162cf1f7baf6b5bc0972e32c3
tea0112 <ducthaidev@gmail.com>
2023-06-07 14:35:44 +0700

    init

---
+++ src/main/java/com/omnicommerce/OmnicommerceApplication.java
@@ -0,0 +1,13 @@
+package com.omnicommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnicommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnicommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omnicommerce/OmnicommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omnicommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnicommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
